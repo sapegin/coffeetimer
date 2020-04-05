d74eead37e37ec7b7ee7735a497fa865c131200c
Artem Sapegin <artem@sapegin.ru>
2020-04-05 16:50:29 +0200

    Add Apple touch icon

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -12,6 +12,8 @@ export const App: React.FC = ({ children }) => (
 				rel="icon"
 				href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>☕️</text></svg>"
 			/>
+			<link rel="apple-touch-icon" href="/icon.png" />
+			<meta name="apple-mobile-web-app-capable" content="yes" />
 		</Head>
 		{children}
 	</Provider>
