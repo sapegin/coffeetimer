889ef7a2648cb42fbea93b297d7bd641f2adb75f
Artem Sapegin <artem@sapegin.ru>
2020-04-05 17:16:17 +0200

    I don't like black status bar

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -14,6 +14,7 @@ export const App: React.FC = ({ children }) => (
 			/>
 			<link rel="apple-touch-icon" href="/icon.png" />
 			<meta name="apple-mobile-web-app-capable" content="yes" />
+			<meta name="apple-mobile-web-app-status-bar-style" content="default" />
 		</Head>
 		{children}
 	</Provider>
