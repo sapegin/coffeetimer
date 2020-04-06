ab2125d5c4a297f969c11c6efeef13d038286a25
Artem Sapegin <artem@sapegin.ru>
2020-04-06 09:49:10 +0200

    Add one more step

--- src/presets/default.tsx
+++ src/presets/default.tsx
@@ -11,13 +11,14 @@ export const preset: BrewPreset = {
 		const ratio = 18;
 		const coffeeAmount = Math.round(waterAmout / ratio);
 		const firstStep = waterAmout / 5;
-		const step = (waterAmout - firstStep) / 3;
+		const step = (waterAmout - firstStep) / 4;
 		return {
 			timer,
 			coffeeAmount,
 			steps: [
 				`Pour ${round(firstStep)} ml of water and wait ${timer} seconds`,
 				`Pour ${round(step)} ml of water and stir the coffee with a spoon`,
+				`Pour ${round(step)} ml more`,
 				`Pour ${round(step)} ml more`,
 				`Pour the rest of the water`,
 				`Shake the jug`,
