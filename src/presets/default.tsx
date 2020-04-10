462ba450269e9be301132b96bfd8f25e5e786db4
Artem Sapegin <artem@sapegin.ru>
2020-04-10 17:01:55 +0200

    Tweak the recipe

--- src/presets/default.tsx
+++ src/presets/default.tsx
@@ -7,16 +7,16 @@ export const preset: BrewPreset = {
 	waterTo: 600,
 	waterDefault: 500,
 	brew: ({ waterAmout }) => {
-		const timer = 30;
+		const timer = 45;
 		const ratio = 18;
 		const coffeeAmount = Math.round(waterAmout / ratio);
-		const firstStep = waterAmout / 5;
-		const step = (waterAmout - firstStep) / 4;
+		const bloom = round(coffeeAmount * 3);
+		const step = (waterAmout - bloom) / 4;
 		return {
 			timer,
 			coffeeAmount,
 			steps: [
-				`Pour ${round(firstStep)} ml of water and wait ${timer} seconds`,
+				`Pour ${round(bloom)} ml of water and wait ${timer} seconds`,
 				`Pour ${round(step)} ml of water and stir the coffee with a spoon`,
 				`Pour ${round(step)} ml more`,
 				`Pour ${round(step)} ml more`,
