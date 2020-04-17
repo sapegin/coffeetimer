75d0f55a6408339c4e6ba59d2d4be1276de9417c
Artem Sapegin <artem@sapegin.ru>
2020-04-17 08:56:10 +0200

    Looks like it's finally working!
    Thanks @lukeed!

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ServerStyleSheet, StyleSheetManager } from 'styled-components';
 import { IndexPage } from './pages/index';
 
-ReactDOM.render(<IndexPage />, document.getElementById('app'));
+declare global {
+	interface Window {
+		PWA_EXPORT?: boolean;
+	}
+}
 
-// if (process.env.NODE_ENV === 'production') {
-// 	// Service Worker registration
-// 	if ('serviceWorker' in navigator) {
-// 		navigator.serviceWorker.register('/sw.js');
-// 	}
-// }
+const container = document.getElementById('app');
+
+if (window.PWA_EXPORT) {
+	const sheet = new ServerStyleSheet();
+
+	ReactDOM.hydrate(
+		<StyleSheetManager sheet={sheet.instance}>
+			<IndexPage />
+		</StyleSheetManager>,
+		container,
+		() => {
+			document.head.innerHTML += sheet.getStyleTags();
+			sheet.seal();
+		}
+	);
+} else if (process.env.NODE_ENV === 'production') {
+	// Service Worker registration
+	// if ('serviceWorker' in navigator) {
+	// 	navigator.serviceWorker.register('/sw.js');
+	// }
+	ReactDOM.hydrate(<IndexPage />, container);
+} else {
+	ReactDOM.render(<IndexPage />, container);
+}
